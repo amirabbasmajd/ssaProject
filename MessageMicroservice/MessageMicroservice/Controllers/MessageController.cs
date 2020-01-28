2079ccb485473a4f758ab25d0a9e44172285fb96
Amirabbas Majd <amirabbasmajd@yahoo.com>
2020-01-28 15:52:00 +0330

    init commit

---
+++ MessageMicroservice/MessageMicroservice/Controllers/MessageController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using MessageService.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+namespace MessageService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MessageController : ControllerBase
+    {
+        private static readonly List<Message>  Messages = new List<Message>
+        {
+            new Message("hi",1,2,0), new Message("hello",2,1,0), new Message("bye",1,2,0)
+        };
+
+        //private readonly ILogger<MessageController> _logger;
+
+        //public MessageController(ILogger<MessageController> logger)
+        //{
+        //    _logger = logger;
+        //}
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Message>> GetAll()
+        {
+            return Messages;
+        }
+
+
+        [HttpGet("{id}")]
+        public ActionResult<Message> GetById(int id)
+        {
+            return Messages.Find(msg => msg.Id == id);
+        }
+
+        [HttpPost]
+        public ActionResult<Message> NewMessage(String content,long sender,long receiver,long chatid)
+        {
+            var nmsg = new Message(content,sender,receiver,chatid);
+            Messages.Add(nmsg);
+            var resourceUrl = Path.Combine(Request.Path.ToString(), Uri.EscapeUriString(nmsg.Content));
+            return Created(resourceUrl, nmsg);
+
+        }
+    }
+}
---
+++ MessageMicroservice/MessageMicroservice/Models/Message.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MessageService.Models
+{
+    public class Message
+    {
+        public long Id { get; set; }
+        public string Content { get; set; }
+        public static long lastid = 0;
+        public long SenderId { get; set; }
+        public long RecieverId { get; set; }
+        public long ChatId { get; set; }
+        public DateTime CreationTime { get; set; }
+        public DateTime LastModifiedTime { get; set; }
+
+        public Message(string content,long sender,long receiver,long chatid)
+        {
+            Id = lastid++;
+            Content = content;
+            CreationTime = DateTime.Now;
+            LastModifiedTime = DateTime.Now;
+            SenderId = sender;
+            RecieverId = receiver;
+            ChatId = chatid;
+        }
+    }
+}
